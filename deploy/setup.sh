3c1065a7bb5c8a21e5fcb51864ba2f8d83fa43ea
Rus, Carol <carol.rus@f-secure.com>
2020-08-24 23:37:11 +0300

    Fix setup script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -8,6 +8,7 @@ PROJECT_GIT_URL='https://github.com/carolrus/profiles-rest-api.git'
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
